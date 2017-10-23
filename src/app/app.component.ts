7f3aa204b97b5334a9e3298fd4f3fbb72b7e558b
Josh Lavely <josh.lavely@gmail.com>
2017-10-22 21:08:26 -0400

    Cleaned code up. Need to move to ES6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component, HostListener, OnInit, AfterViewInit, ElementRef, ViewChild  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
   @ViewChild('name') name;
   title = 'ng-Snake';
@@ -64,7 +66,7 @@ ngOnInit() {
   this.ctx = this.canvas.getContext("2d");
   this.w = this.canvas.width;
   this.h = this.canvas.height;
-
+  
   // sort scores
   this.highScores = this.highScores.sort((a,b) => b.score - a.score);
     // begin game
@@ -73,19 +75,20 @@ ngOnInit() {
 
 public openMyDialog(){
     let myDialog:any = <any>document.getElementById("modalDialog");
-    myDialog.showModal();    }
-public closeDialog()
-{
-  let myDialog:any = <any>document.getElementById("modalDialog");
-  myDialog.close('cancelling');
-}
+    myDialog.showModal();    
+  }
+    
+  public closeDialog(){
+    let myDialog:any = <any>document.getElementById("modalDialog");
+    myDialog.close('cancelling');
+  }
 
-public saveScore() {
-  this.highScores.push({name: this.name.nativeElement.value, score: this.score});
-  this.highScores = this.highScores.sort((a,b) => b.score - a.score);
-  let myDialog:any = <any>document.getElementById("modalDialog");
-  myDialog.close('Saving Score');
-}
+  public saveScore() {
+    this.highScores.push({name: this.name.nativeElement.value, score: this.score});
+    this.highScores = this.highScores.sort((a,b) => b.score - a.score);
+    let myDialog:any = <any>document.getElementById("modalDialog");
+    myDialog.close('Saving Score');
+  }
 
   public pauseGame(){
     console.log("pausing game",this.isPaused)
@@ -155,76 +158,80 @@ public paint() {
   //The movement code for the snake to come here.
   //The logic is simple
   //Pop out the tail cell and place it infront of the head cell
-  var nx = this.snake_array[0].x;
-  var ny = this.snake_array[0].y;
+      var nx = this.snake_array[0].x;
+      var ny = this.snake_array[0].y;
   //These were the position of the head cell.
   //We will increment it to get the new head position
   //Lets add proper direction based movement now
-  if (this.d == "right") nx++;
-  else if (this.d == "left") nx--;
-  else if (this.d == "up") ny--;
-  else if (this.d == "down") ny++;
+      if (this.d == "right") nx++;
+      else if (this.d == "left") nx--;
+      else if (this.d == "up") ny--;
+      else if (this.d == "down") ny++;
 
-  //Lets add the game over clauses now
-  //This will restart the game if the snake hits the wall
   //Lets add the code for body collision
   //Now if the head of the snake bumps into its body, the game will restart
-  if (nx == -1 || nx == this.w / this.cw || ny == -1 || ny == this.h / this.cw || this.check_collision(nx, ny, this.snake_array)) {
+    if (nx == -1 || nx == this.w / this.cw || ny == -1 || ny == this.h / this.cw || this.check_collision(nx, ny, this.snake_array)) {
 
-    if(this.lives === 0){
-      //restart game
-      //alert("Ran Out of Lives!!")
-      this.openMyDialog();
-      //Lets organize the code a bit now.
-      this.pauseGame();
+      if(this.lives === 0){
+        this.openMyDialog();
+        this.pauseGame();
 
-      return;
+        return;
+      }
+      else{
+        this.lives--;
+        this.init();
+        return;
+      }
     }
-    else{
-      this.lives--;
-      this.init();
-      return;
-    }
-  }
 
   //Lets write the code to make the snake eat the food
   //The logic is simple
   //If the new head position matches with that of the food,
   //Create a new head instead of moving the tail
-  if (nx == this.food.x && ny == this.food.y) {
-      var tail: any = {
-          x: nx,
-          y: ny
-      };
-      this.score++;
-      //Create new food
-      this.create_food();
-  }
-  else {
-      var tail: any = this.snake_array.pop(); //pops out the last cell
-      tail.x = nx;
-      tail.y = ny;
-  }
+      if (nx == this.food.x && ny == this.food.y) {
+          var tail: any = {
+              x: nx,
+              y: ny
+          };
+          this.score++;
+          //Create new food
+          this.create_food();
+      }
+      else {
+          var tail: any = this.snake_array.pop(); //pops out the last cell
+          tail.x = nx;
+          tail.y = ny;
+      }
   //The snake can now eat the food.
-  this.snake_array.unshift(tail); //puts back the tail as the first cell
-  for (var i = 0; i < this.snake_array.length; i++) {
-      var c = this.snake_array[i];
-      //Lets paint 10px wide cells
-      this.paint_cell(c.x, c.y);
-  }
+      this.snake_array.unshift(tail); //puts back the tail as the first cell
+      for (var i = 0; i < this.snake_array.length; i++) {
+          var c = this.snake_array[i];
+          //Lets paint 10px wide cells
+          this.paint_cell(c.x, c.y);
+      }
 
   //Lets paint the food
-  this.paint_cell(this.food.x, this.food.y);
+      this.paint_cell(this.food.x, this.food.y);
   //Lets paint the score
-  var score_text = "Score: " + this.score;
-  this.ctx.fillText(score_text, 5, this.h - 5);
-  // Paint lives
-  var lives_text = "Lives: " + this.lives;
-  this.ctx.fillText(lives_text, 400, this.h - 5);
-}
+      // Moved score and lives to angular component, leaving this off canvas.
+        /* var score_text = "Score: " + this.score;
+        this.ctx.fillText(score_text, 5, this.h - 5);
+        // Paint lives
+        var lives_text = "Lives: " + this.lives;
+        this.ctx.fillText(lives_text, 400, this.h - 5); */
+  }
 }
 
-//Lets first create a generic function to paint cells
+//Lets first create a generic function to paint on canvas the food
+  // paint_food i am looking at getting svg painted into the cell for added UI
+public paint_food(x,y){
+  var img = new Image();
+  img.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==";
+  img.height = 5;
+  img.width = 5;
+  this.ctx.drawImage(img,x,y)
+}
 
 public paint_cell(x, y) {
   this.ctx.fillStyle = "blue";
